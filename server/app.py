4eda69e5719aa70f17291eddff27e659ab39d30e
Martin <martingarcia804@yahoo.com>
2024-01-22 11:11:29 -0600

    Finished

--- server/app.py
+++ server/app.py
@@ -69,13 +69,36 @@ api.add_resource(Newsletters, '/newsletters')
 
 class NewsletterByID(Resource):
 
-    def get(self, id):
+    # get()
 
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+    def patch(self, id):
+
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
 
         response = make_response(
             response_dict,
-            200,
+            200
+        )
+
+    def delete(self, id):
+
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
         )
 
         return response
